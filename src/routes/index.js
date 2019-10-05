4a56f9e287f98307e112f903d1b4db441eb95d59
ashudeshwal999 <ashudeshwal999@gmail.com>
2019-10-05 18:03:00 +0530

    --update : add counter in redis cache

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 import {Router} from 'express';
 import { sendResponse, handleError } from '../utils';
 import { responseCode } from '../constants';
+import { saveLongUrl } from '../servies';
 
 const router = Router();
 
-router.route('/').get(async(req, res) => {
+router.route('/shortUrl').get(async(req, res) => {
   try{
+    const { longUrl } = req.body;
+    if (!longUrl){
+      return sendResponse(res, responseCode.UNPROCESSABLE_ENTITY.code,{},responseCode.UNPROCESSABLE_ENTITY.msg);
+    }
+    await saveLongUrl({
+      longUrl,
+    });
     return sendResponse(res, responseCode.OK.code, 'Good Day', responseCode.OK.msg);
   }
   catch(err){
