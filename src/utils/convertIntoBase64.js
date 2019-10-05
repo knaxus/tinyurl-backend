de0927b2ffbfe49125ef3d996625fb4c022f24e9
ashudeshwal999 <ashudeshwal999@gmail.com>
2019-10-05 14:24:53 +0530

    --update : add try/catch

--- src/utils/convertIntoBase64.js
+++ src/utils/convertIntoBase64.js
@@ -1,10 +1,19 @@
+import { customErrorThrow } from '../utils';
 export function convertIntoBase64(number){
-  const options = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let hashStr = '';
-  while(number>0){
-    const remainder = number%62;
-    hashStr = options[remainder] + hashStr;
-    number/=62;
+  try{
+    const options = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let hashStr = '';
+    while(number>0){
+      const remainder = number%62;
+      hashStr = options[remainder] + hashStr;
+      number=Math.floor(number/62);
+    }
+    if(hashStr.length>7){
+      customErrorThrow(400, 'Hash generated length is greater than 7')
+    }
+    return hashStr;
   }
-  return hashStr;
+  catch(err){
+    throw err;
+ }
 }
